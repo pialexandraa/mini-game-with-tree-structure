bd3a3e2b28e19b2a13427d0c9daca9c7c3a11485
petrachealexandra <>
2025-09-20 23:34:53 +0200

    Additing more files

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ def main():
     print("Welcomg to the game!")
 
     #check for user agreement
-    user_input = input("Are you ready to start? (yes/no)\n")
+    user_input = input("Are you ready to start? (yes/no) ")
     if user_input.lower() == "no":
       print("Sad to see you go. Stopping the game.")
       exit_program()
     elif user_input.lower() != "yes":
-      print("Invalid option. This is not yes/no.")
+      print("Invalid option. This is not a 'yes/no' answer.")
       exit_program()
     else:
       pass
@@ -45,16 +45,16 @@ class TreeNode:
   def traverse(self):
     story_node = self
     print(story_node.story_piece)
-
     while len(story_node.choices) > 0:
       choice = input("Enter 1 or 2 to continue the story: \n")
       if choice not in ["1", "2"]:
-        print("Please, enter a valid choice: ")
+        print("Invalid choice. Try again")
       else:
         chosen_index = int(choice)
         chosen_index -= 1
         chosen_child = story_node.choices[chosen_index]
         print(chosen_child.story_piece)
+        story_node = chosen_child
 
 
 story_root = TreeNode("""
@@ -66,43 +66,42 @@ Do you:
 """)
 
 choice_a = TreeNode("""
-The bear is startled and just runs away.
+The bear is scared on his end, and he just runs away.
 Do you:
 1 ) Shout 'Sorry bear!'
-2 ) Yell 'Hooray!'
+2 ) Happily say 'Hooray!'
 """)
 
 choice_b = TreeNode("""
 After turning to the left, you come across a clearing full of flowers. 
-The bear followed you but now he starts talking; 'what is going on?, he asks you'
+The bear followed you but now he starts talking; 'what is going on?', he asks you.
 Do you:
 1 ) Gasp 'Wow, a talking bear!'
-2 ) Explain to him that he scared you, and that you are looking for an escape.
+2 ) Explain that you are scared, and that you are looking for an escape.
 """)
 
 story_root.add_child(choice_a)
 story_root.add_child(choice_b)
-story_root.traverse()
+#story_root.traverse()
 
 choice_a_1 = TreeNode("""
-The bear returns and he is talking. Wow, a talking bear! However, in the end, he helps you and shows you the way out of the forest.
+The bear returns and he seems to be saying something. Wow, a talking bear! In the end, he helps you and shows you the way out of the forest.
 
 YOU HAVE ESCAPED THE WILDERNESS.
 """)
 
 choice_a_2 = TreeNode("""
-The bear returns roars one more time at you meanly, and then he leaves you alone
-in the wilderness.
+The bear returns, roars one more time at you meanly, and then he leaves you alone in the wilderness.
 
 YOU REMAIN LOST.
 """)
 
 choice_a.add_child(choice_a_1)
 choice_a.add_child(choice_a_2)
-choice_a.traverse()
+#choice_a.traverse()
 
 choice_b_1 = TreeNode("""
-The bear is unamused by your remark. After smelling the flowers, he just turns around and leaves you alone.
+The bear is unamused by your remark. He turns around and leaves you alone.
 
 YOU REMAIN LOST.
 """)
@@ -115,8 +114,7 @@ YOU HAVE ESCAPED THE WILDERNESS.
 """)
 choice_b.add_child(choice_b_1)
 choice_b.add_child(choice_b_2)
-choice_b.traverse()
+#choice_b.traverse()
+story_root.traverse()
 
-print(story_root.story_piece)
-#user_choice = input("What is your name?\n")
-#print(user_choice)
+exit_program()
